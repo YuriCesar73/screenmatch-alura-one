244398c26b7b006db80e1d2554b9fb795933db45
Yuri Cesar <ycesar4273@gmail.com>
2025-06-20 11:44:46 -0300

    Usando try catch

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -18,38 +18,45 @@ import br.com.alura.screenmatch.modelos.TituloOmdb;
 public class PrincipalComBusca {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
+
 		Scanner leitura = new Scanner(System.in);
 		System.out.println("Digite o nome do filme");
-		
+
 		var busca = leitura.nextLine();
 		String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=5e9b0ca8";
-		
-		HttpClient client = HttpClient.newHttpClient();
+		try {			
+			HttpClient client = HttpClient.newHttpClient();
 
-		HttpRequest request = HttpRequest.newBuilder()
-		      .uri(URI.create(endereco))
-		      .build();
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(endereco))
+					.build();
 
-		HttpResponse<String> response = client
-				  .send(request, BodyHandlers.ofString());
-		
-		String json = response.body();
-		System.out.println(json);
-		
-		Gson gson = new GsonBuilder().
-				setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).
-				create();
-		
-		
-		
-		TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-		System.out.println(meuTituloOmdb);
-		
-		Titulo meuTitulo = new Titulo(meuTituloOmdb);
-		System.out.println(meuTitulo);
+			HttpResponse<String> response = client
+					.send(request, BodyHandlers.ofString());
+
+			String json = response.body();
+			System.out.println(json);
+
+			Gson gson = new GsonBuilder().
+					setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).
+					create();
+
+
+
+			TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+			System.out.println(meuTituloOmdb);
+
+
+			Titulo meuTitulo = new Titulo(meuTituloOmdb);
+			System.out.println(meuTitulo);
+		} catch (NumberFormatException e) {
+			System.out.println("Aconteceu um erro");
+			System.out.println(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.out.println("Algum erro de argumento na busca, verifique o endereço");
+		}
+
 
 	}
 
 }
-	
